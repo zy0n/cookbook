c5e26c819c9fa799e649ef8b93bfa4e56a5a7b6f
zy0n.bear <zy0n@users.noreply.github.com>
2023-05-15 16:52:11 -0400

    erc20-multi-transfer

--- src/steps/adapt/transfer-erc20-token-multi-step.ts
+++ src/steps/adapt/transfer-erc20-token-multi-step.ts
@@ -4,11 +4,13 @@ import {
   RecipeERC20AmountRecipient,
   RecipeERC20Info,
   StepInput,
+  StepOutputERC20Amount,
   UnvalidatedStepOutput,
 } from '../../models/export-models';
 import { compareERC20Info } from '../../utils/token';
 import { Step } from '../step';
 import { PopulatedTransaction } from '@ethersproject/contracts';
+import { RelayAdapt } from '../../abi-typechain/adapt/RelayAdapt';
 
 export type ERC20Transfer = {
   toAddress: string;
@@ -23,51 +25,59 @@ export class TransferERC20TokenMultiStep extends Step {
       'Transfers a set of ERC20 tokens to many external public addresses.',
   };
 
-  private readonly toAddress: string;
-  private readonly tokenInfo: RecipeERC20Info;
-  private readonly amount: Optional<BigNumber>;
+  private readonly transfers: ERC20Transfer[];
 
-  constructor(
-    toAddress: string,
-    tokenInfo: RecipeERC20Info,
-    amount?: BigNumber,
-  ) {
+  constructor(erc20TokenTransfers: ERC20Transfer[]) {
     super();
-    this.toAddress = toAddress;
-    (this.tokenInfo = tokenInfo), (this.amount = amount);
+    this.transfers = erc20TokenTransfers;
   }
 
   protected async getStepOutput(
     input: StepInput,
   ): Promise<UnvalidatedStepOutput> {
+    // start off with these inputs, but we'll pull the unusedERC20Amounts out
+    // each loop and replace erc20Amounts with that.
     const { erc20Amounts } = input;
-
-    const { erc20AmountForStep, unusedERC20Amounts } =
-      this.getValidInputERC20Amount(
-        erc20Amounts,
-        erc20Amount => compareERC20Info(erc20Amount, this.tokenInfo),
-        this.amount,
-      );
-
     const contract = new RelayAdaptContract(input.networkName);
-    const populatedTransactions: PopulatedTransaction[] = [
-      await contract.createERC20TokenTransfer(
-        this.toAddress,
-        this.tokenInfo.tokenAddress,
-        this.amount,
-      ),
-    ];
 
-    const transferredBaseToken: RecipeERC20AmountRecipient = {
-      ...this.tokenInfo,
-      amount: this.amount ?? erc20AmountForStep.expectedBalance,
-      recipient: this.toAddress,
-    };
+    const spentERC20Amounts: RecipeERC20AmountRecipient[] = [];
+    const erc20TokenTransfers: RelayAdapt.TokenTransferStruct[] = [];
+    let lastUnusedERC20Amounts: StepOutputERC20Amount[] = erc20Amounts;
+
+    for (const transfer of this.transfers) {
+      const { toAddress, tokenInfo, amount } = transfer;
+
+      const { erc20AmountForStep, unusedERC20Amounts } =
+        this.getValidInputERC20Amount(
+          lastUnusedERC20Amounts,
+          erc20Amount => compareERC20Info(erc20Amount, tokenInfo),
+          amount,
+        );
+
+      const erc20TokenTransfer = contract.createERC20TokenTransferStruct(
+        toAddress,
+        tokenInfo.tokenAddress,
+        amount,
+      );
+      erc20TokenTransfers.push(erc20TokenTransfer);
+
+      const transferredERC20Token: RecipeERC20AmountRecipient = {
+        ...tokenInfo,
+        amount: amount ?? erc20AmountForStep.expectedBalance,
+        recipient: toAddress,
+      };
+      spentERC20Amounts.push(transferredERC20Token);
+      lastUnusedERC20Amounts = unusedERC20Amounts;
+    }
+
+    const populatedTransactions: PopulatedTransaction[] = [
+      await contract.createMultiERC20TokenTransfer(erc20TokenTransfers),
+    ];
 
     return {
       populatedTransactions,
-      spentERC20Amounts: [transferredBaseToken],
-      outputERC20Amounts: unusedERC20Amounts,
+      spentERC20Amounts,
+      outputERC20Amounts: lastUnusedERC20Amounts,
       spentNFTs: [],
       outputNFTs: input.nfts,
       feeERC20AmountRecipients: [],
